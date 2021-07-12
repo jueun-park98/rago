2a2b47bb093c537587c5504738f737a819ea2d80
jueun.park <s5616012@msx.tu-dresden.de>
2021-07-13 04:20:07 +0900

    edit

--- src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
+++ src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
@@ -1,10 +1,10 @@
 package de.tudresden.inf.st.openapi;
 
-import de.tudresden.inf.st.openapi.ast.OpenAPIObject;
+import de.tudresden.inf.st.openapi.ast.*;
 import org.openapi4j.core.exception.ResolutionException;
 import org.openapi4j.core.validation.ValidationException;
 import org.openapi4j.parser.OpenApi3Parser;
-import org.openapi4j.parser.model.v3.OpenApi3;
+import org.openapi4j.parser.model.v3.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,11 +12,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 
 public class OpenAPIMain {
 
     /** saving json-file with the openapi4j-parser in OpenAPIObject from RAG **/
-    public static OpenAPIObject setJSONinOpenAPIObj(String fileName) throws IOException, ResolutionException, ValidationException {
+    public static OpenAPIObject parseJSON(String fileName) throws IOException, ResolutionException, ValidationException {
         URL expUrl = OpenAPIMain.class.getClassLoader().getResource(fileName);
         File file = null;
         if (expUrl != null) {
@@ -31,8 +34,93 @@ public class OpenAPIMain {
         System.out.println("Loading expression DSL file '" + fileName + "'.");
         FileReader reader = new FileReader(file);
         OpenApi3 api = new OpenApi3Parser().parse(expUrl, new ArrayList<>(), false);
-        System.out.println(api.toString());
-        return (OpenAPIObject) null; // ToDo
+        OpenAPIObject openapi = new OpenAPIObject();
+
+        /** OpenAPI Object **/
+        openapi.setOpenAPI(api.getOpenapi());
+
+        /** Contact Object **/
+        ContactObject contact = new ContactObject(  api.getInfo().getContact().getName(),
+                                                    api.getInfo().getContact().getUrl(),
+                                                    api.getInfo().getContact().getEmail());
+
+        /** License Object **/
+        LicenseObject license = new LicenseObject(  api.getInfo().getLicense().getName(),
+                                                    api.getInfo().getLicense().getUrl());
+
+        /** Info Object **/
+        openapi.setInfoObject( new InfoObject(  api.getInfo().getTitle(),
+                                                api.getInfo().getDescription(),
+                                                api.getInfo().getTermsOfService(),
+                                                new Opt<>(contact),
+                                                new Opt<>(license),
+                                                api.getInfo().getVersion()));
+
+        /** Server Object **/
+
+        /** Server Variable Object **/
+
+        /** Components Object **/
+
+        /** Paths Object **/
+
+        /** Path Item Object **/
+
+        /** Operation Object **/
+
+        /** External Documentation Object **/
+
+        /** Parameter Object **/
+
+        /** Request Body Object **/
+
+        /** Media Type Object **/
+
+        /** Encoding Object **/
+
+        /** Responses Object**/
+
+        /** Response Object **/
+
+        /** Callback Object **/
+
+        /** Example Object **/
+
+        /** Link Object **/
+
+        /** Header Object**/
+
+        /** Tag Object **/
+
+        /** Reference Object **/
+
+        /** Schema Object **/
+
+        /** Discriminator Object **/
+
+        /** XML Object **/
+
+        /** Security Scheme Object **/
+
+        /** OAuth Flows Object **/
+        /*
+        OAuthFlowsObject oAuthFlows;
+        oAuthFlows.setImplicitOpt(new Opt<>(new Implicit(api)));
+        oAuthFlows.setPasswordOpt();
+        oAuthFlows.setClientCredentialsOpt();
+        oAuthFlows.setAuthorizationCodeOpt();*/
+
+        /** OAuth Flow Object **/
+
+        /** Security Requirement Object **/
+        /*
+        for( SecurityRequirement s : api.getSecurityRequirements() ){
+            openapi.addSecurityRequirementObject(new SecurityRequirementObject(s., s));
+            for( s.getRequirements(). )
+            openapi.addSecurityRequirementObject(new SecurityRequirementObject(s.getRequirements().));
+        } */
+
+        return openapi;
     }
 
     /** Result of Print.jrag in String will be saved in json-format **/
